854f90099db9a07f64c44074373076cebf367dae
xRokus <rafael_cintra_@hotmail.com>
2020-06-27 17:49:08 -0300

    Sistema de movimentação do personagem

---
+++ Journey-1th/Assets/Code/Character.cs
@@ -0,0 +1,42 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Rigidbody2D rb;
+
+    public CharacterStates characterStates;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && characterStates == CharacterStates.OnGround)
+        {
+            rb.AddForce(new Vector2(0f, 4f), ForceMode2D.Impulse);
+            characterStates = CharacterStates.Jumping;
+        }
+    }
+}
+
+public enum CharacterStates
+{
+    OnGround,
+    Jumping,
+    DoubleJumping
+}
---
+++ Journey-1th/Assets/GroundCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public Character character;  
+
+    private void OnCollisionEnter2D(Collision2D collision) //Quando entra
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            character.characterStates = CharacterStates.OnGround; // Se o personagem ta em contato com o chão ele chama o script Character.
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision) // Quando sai da colisão
+    {
+        if (collision.collider.tag == "Ground")
+        {
+         //   character.characterStates = CharacterStates.Jumping;
+        }
+    }
+}
